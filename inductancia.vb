72cd7f9bae3c6781f1bf72f3b6c9ae0ba653940e
laulaupardo19 <lautaro15emanuel@gmail.com>
2024-08-06 19:45:32 -0300

    tarea lista

---
+++ inductancia.vb
@@ -0,0 +1,16 @@
+﻿Module module
+    Sub main()
+        Dim frecuencia As Double
+        Dim inductancia As Double
+        Dim impedancia As Double
+
+
+        console.write("ingrese la frecuencia (Hz): ")
+        frecuencia = console.readline()
+        console.write("ingrese el valor de la inducatancia (h)")
+        inductancia = console.readline()
+        impedancia = 2 * XL pi + frecuencia + inductancia
+        console.writeline("la impedancia indictiva es:" & impedancia)
+
+    End Sub
+End Module
